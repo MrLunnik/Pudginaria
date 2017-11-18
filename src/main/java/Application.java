10af53d023231fea0d104d591583d71377146792
Andrey Biryukov <andrey.biryukov@raiffeisen.ru>
2017-11-18 23:14:51 +0300

    rename

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,8 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
-
+public class Application {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
